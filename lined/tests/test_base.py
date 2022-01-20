47eb787a4c92fe7abfb3867859581eacddaaedb1
Christian Avart <cavart28@gmail.com>
2022-01-20 11:31:58 -0500

    Reformat code to pass CI

--- lined/tests/test_base.py
+++ lined/tests/test_base.py
@@ -1,13 +1,22 @@
+"""Tests for base.py"""
+
 from lined.base import _merge_funcs_and_named_funcs
 import pytest
 
-class my_class():
+
+class my_class:
+    """A simple class with one method"""
+
     def add_one(self, x):
         return x + 1
 
-class my_other_class():
+
+class my_other_class:
+    """Another simple class with one method"""
+
     def add_one(self, x):
         return x + 2
+
 
 first = my_class()
 f = first.add_one
@@ -20,12 +29,24 @@ h = third.add_one
 
 i = lambda x: 2 * x
 
+
 def j(x: int):
     return 2 * x + 1
 
+
 @pytest.mark.parametrize(
     'funcs_tuple',
-    [(f), (f, g), (f, g, h), (f, g, h, i), (f, g, h, i, j), (f, g, h, i, j, f, g, h, i, j)])
+    [
+        (f),
+        (f, g),
+        (f, g, h),
+        (f, g, h, i),
+        (f, g, h, i, j),
+        (f, g, h, i, j, f, g, h, i, j),
+    ],
+)
 def test_merge_funcs_and_named_funcs(funcs_tuple):
-    assert len(_merge_funcs_and_named_funcs(funcs_tuple, named_funcs={})) == len(funcs_tuple), "Merging resulted in losing at least one function"
-
+    """Test that no function is lost after merging"""
+    assert len(
+        _merge_funcs_and_named_funcs(funcs_tuple, named_funcs={})
+    ) == len(funcs_tuple), 'Merging resulted in losing at least one function'
